39dd52c716ea5873cdfa6a50fd4af1bf8d448e43
Haite913 <326085389@qq.com>
2023-11-28 22:50:33 +0800

    登陆功能token实现

--- src/main/java/com/team/ptjs/Biz/service/impl/UserServiceImpl.java
+++ src/main/java/com/team/ptjs/Biz/service/impl/UserServiceImpl.java
@@ -3,10 +3,14 @@ package com.team.ptjs.Biz.service.impl;
 
 import com.team.ptjs.Api.R.R;
 import com.team.ptjs.Api.dto.UserDto;
+import com.team.ptjs.Api.entity.User;
 import com.team.ptjs.Biz.mapper.UserMapper;
 import com.team.ptjs.Biz.service.UserService;
+import com.team.ptjs.Utils.jwt.TokenUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,7 +29,10 @@ public class UserServiceImpl implements UserService {
     public R Login(UserDto userDto) {
         String password = userMapper.getPassword(userDto.getUsername());
         if(password!=null && password!="" && userDto.getPassword().equals(password)){
-            return R.ok(0,"登陆成功");
+            User user = new User();
+            BeanUtils.copyProperties(userDto,user);
+            String token = TokenUtil.sign(user);
+            return R.ok(0,token);
         }else{
             return R.failed(1,"登陆失败");
         }
---
+++ src/main/java/com/team/ptjs/Utils/jwt/TokenUtil.java
@@ -0,0 +1,55 @@
+package com.team.ptjs.Utils.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.team.ptjs.Api.entity.User;
+
+
+import java.util.Date;
+
+
+public class TokenUtil {
+    private static final long EXPIRE_TIME= 24*60*60*1000;   //十小时
+    private static final String TOKEN_SECRET="123456";  //密钥盐
+    /**
+     * 签名生成
+     * @param user
+     * @return
+     */
+    public static String sign(User user){
+        String token = null;
+        try {
+            Date expiresAt = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+            token = JWT.create()
+                    .withIssuer("auth0")
+                    .withClaim("Username", user.getUsername())
+//                    .withAudience(staff.getUsername())
+                    .withClaim("Password",user.getPassword())
+                    .withExpiresAt(expiresAt)
+                    // 使用了HMAC256加密算法。
+                    .sign(Algorithm.HMAC256(TOKEN_SECRET));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return token;
+    }
+    /**
+     * 签名验证
+     * @param token
+     * @return
+     */
+    public static boolean verify(String token){
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(TOKEN_SECRET)).withIssuer("auth0").build();
+            DecodedJWT jwt = verifier.verify(token);
+            System.out.println("认证通过：");
+            System.out.println("Username: " + jwt.getClaim("Username").asString());
+            System.out.println("过期时间：      " + jwt.getExpiresAt());
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
